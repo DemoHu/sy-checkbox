ff3828bfc02d63b9daaefe14592555533ce6550b
siwen <623746556@qq.com>
2019-09-02 17:51:32 +0800

    注释

--- examples/main.js
+++ examples/main.js
@@ -2,15 +2,16 @@
  * @Author: Siwen
  * @Date: 2019-09-02 16:32:02
  * @LastEditors: Siwen
- * @LastEditTime: 2019-09-02 17:05:32
+ * @LastEditTime: 2019-09-02 17:50:38
  * @Description: 
  */
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-Vue.use(ElementUI);
+// 测试
+// import ElementUI from 'element-ui';
+// import 'element-ui/lib/theme-chalk/index.css';
+// Vue.use(ElementUI);
 
 import syCheckbox from './../packages/index'
 Vue.use(syCheckbox)
